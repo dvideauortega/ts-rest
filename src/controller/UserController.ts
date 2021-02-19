a56627f07f5f3e3529abadf49fd1dba635d2338e
David Videau Ortega <d.videau.ortega@gmail.com>
2021-02-19 14:29:07 -0300

    Changed router access on controller and controller registration parameter type

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -5,11 +5,36 @@ import Controller from "./Controller";
 class UserController extends Controller {
 
     private findAll(request: Request, response: Response, next: NextFunction): void {
-        response.end("Hello from users controller");
+        response.end(`${request.method} ${request.url}`);
+    }
+
+    private findById(request: Request, response: Response, next: NextFunction): void {
+        response.end(`${request.method} ${request.url}`);
+    }
+
+    private save(request: Request, response: Response, next: NextFunction): void {
+        response.end(`${request.method} ${request.url}`);
+    }
+
+    private replace(request: Request, response: Response, next: NextFunction): void {
+        response.end(`${request.method} ${request.url}`);
+    }
+
+    private update(request: Request, response: Response, next: NextFunction): void {
+        response.end(`${request.method} ${request.url}`);
+    }
+
+    private delete(request: Request, response: Response, next: NextFunction): void {
+        response.end(`${request.method} ${request.url}`);
     }
 
     public configure(router: Router): void {
         router.get("/", this.findAll);
+        router.get("/:id", this.findById);
+        router.post("/", this.save);
+        router.put("/", this.replace);
+        router.patch("/", this.update);
+        router.delete("/", this.delete);
     }
 
 }
