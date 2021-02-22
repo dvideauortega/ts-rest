1dd44cacb03629cb838e791fcb8124c5b19d6b5b
David Videau Ortega <d.videau.ortega@gmail.com>
2021-02-22 17:40:27 -0300

    Added test suite and assertion library

---
+++ src/test/entities/other.test.ts
@@ -0,0 +1,9 @@
+import * as chai from 'chai';
+
+describe('Subtraction tests', () => { // the tests container
+    it('2-2 = 0', () => {
+        chai.expect(2-2).to.be.equal(0);
+        chai.assert(2 + 2 == 4, "Two plus two should be equals to four")
+    });
+
+});
