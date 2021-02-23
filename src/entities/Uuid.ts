b54e2dd54649c6d45910040475b367905c4f5f06
David Videau Ortega <d.videau.ortega@gmail.com>
2021-02-23 19:09:57 -0300

    Improved UUID conversions. They now use uuid built-in functions instead of manual string parsing and buffer conversion

--- src/entities/Uuid.ts
+++ src/entities/Uuid.ts
@@ -1,22 +1,48 @@
 import * as uuid from "uuid";
 
 class Uuid {
-    private stringDashedId: string;
-    private stringId: string;
-    private bufferedId: Buffer;
     
-    constructor() {
-        this.stringDashedId = uuid.v4();
-        this.stringId = this.stringDashedId.split("-").join("");
-        this.bufferedId = Buffer.alloc(16, this.stringId, "hex");
+    private stringId: string = "";
+    private bufferedId: Buffer = Buffer.alloc(16);
+    
+    constructor(uid?: string | Buffer) {
+        
+        if (!uid) {
+            this.stringId = uuid.v4();
+            this.bufferedId = this.stringToBuffer(this.stringId);
+        }
+
+        if (uid instanceof Buffer) {
+            this.bufferedId = uid;
+            this.stringId = this.bufferToString(uid);
+        } 
+        
+        if (typeof uid === "string") {
+            this.stringId = uid;
+            this.bufferedId = this.stringToBuffer(uid); 
+        }
+        
+    }
+
+    private stringToBuffer(uuidString: string): Buffer {
+        
+        if (!uuid.validate(uuidString)) 
+            throw new Error("Invalid UUID string.");
+        
+        let bytesArray: ArrayLike<number> | Uint8Array = uuid.parse(uuidString);
+        let uintArray: Uint8Array = new Uint8Array(bytesArray);
+        
+        return Buffer.from(uintArray.buffer);
+    }
+
+    private bufferToString(uuidBuffer: Buffer) {
+        let uidString = uuid.stringify(uuidBuffer);
+        if (uuid.validate(uidString)) return uidString;
+        else throw new Error("Invalid UUID buffer.");
     }
 
     public getString(): string {
         return this.stringId;
-    }
-
-    public getDashedString(): string {
-        return this.stringDashedId;
     }
 
     public getBuffer(): Buffer {
--- src/test/uuid.test.ts
+++ src/test/uuid.test.ts
@@ -1,26 +1,35 @@
 import { assert } from 'chai';
-import { validate } from 'uuid';
+import { v4, validate } from 'uuid';
 import Uuid from '../entities/Uuid';
 
 describe('UUID', () => {
     
-    it("Should be be a valid buffered UUID of length 16", () => {
-        const id = new Uuid().getBuffer();
-        assert.instanceOf(id, Buffer);
-        assert.lengthOf(id, 16);
-    });
+    describe("Empty constructor", () => {
+        
+        it("Should be be a valid buffered UUID of length 16", () => {
+            const id = new Uuid().getBuffer();
+            assert.instanceOf(id, Buffer);
+            assert.lengthOf(id, 16);
+        });
 
-    it("String without dashes should be of length 32", () => {
-        const id: string = new Uuid().getString();
-        assert.isString(id);
-        assert.lengthOf(id, 32);
-    });
+        it("String with dashes should be of length 36 and correctly validated in uuid library", () => {
+            const id: string = new Uuid().getString();
+            const isValid: boolean = validate(id);
+            assert.lengthOf(id, 36);
+            assert.isTrue(isValid);
+        })
+    
+    })
 
-    it("String with dashes should be of length 36 and correctly validated in uuid library", () => {
-        const id: string = new Uuid().getDashedString();
-        const isValid: boolean = validate(id);
-        assert.lengthOf(id, 36);
-        assert.isTrue(isValid);
+    describe("From existing uuid string", () => {
+
+        it("Should transform standard v4 uuid dashed string to binary 16", () => {
+            const str: string = v4();
+            const uuid: Uuid = new Uuid(str);
+            const buffer: Buffer = uuid.getBuffer();
+            assert.lengthOf(buffer, 16);
+            assert.instanceOf(buffer, Buffer);
+        })
     })
 
 });
